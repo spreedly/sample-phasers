868487cd3cafa8e6712539f06844967ec770ad29
Duff OMelia <duff@omelia.org>
2013-07-26 15:42:30 -0400

    Cleaned out some old junk

--- env.rb
+++ env.rb
@@ -5,8 +5,8 @@ require dotenv if File.exist?(dotenv)
 ENV["RAILS_ENV"] ||= "development"
 
 ENV["DATABASE_ADAPTER"] ||= "postgresql"
-ENV["DATABASE_NAME"] ||= "core_sample"
-ENV["DATABASE_TEST_NAME"] ||= "core_sample_test"
+ENV["DATABASE_NAME"] ||= "spreedly_phasers"
+ENV["DATABASE_TEST_NAME"] ||= "spreedly_phasers_test"
 ENV["DATABASE_USER"] ||= "root"
 ENV["DATABASE_PASSWORD"] ||= ""
 ENV["DATABASE_ENCODING"] ||= "unicode"
@@ -15,11 +15,7 @@ ENV["CORE_ENVIRONMENT_KEY"] ||= "OB9909MNZj62u9U4VAz3lAAPZcp"
 ENV["CORE_ACCESS_SECRET"] ||= "MIMo7gJrSi3LnJGdRrOZeBBowXmDVE4zEgyHQS91tIqZiJ2oiy6PRt5XJVXG7hcz"
 ENV["CORE_DOMAIN"] ||=  "https://spreedlycore.com"
 
-ENV["CORE_GATEWAY_FOR_SPREL"] ||=  "StuXzUJ5Khe3Wes31T0M4uNnjv9"
 ENV["CORE_GATEWAY_FOR_CREDIT_CARD"] ||=  "StuXzUJ5Khe3Wes31T0M4uNnjv9"
-ENV["CORE_GATEWAY_FOR_DWOLLA"] ||=  "rRrNqN3NcFRokpYMvY6TxyPrXDg"
-ENV["CORE_GATEWAY_FOR_PAYPAL"] ||=  "9Y5byWM9nMcas972qfvKEce4eMa"
-ENV["CORE_GATEWAY_FOR_GOCARDLESS"] ||=  "LmeT3035Un1ILf8fiHliLrLr13y"
 
 
 
