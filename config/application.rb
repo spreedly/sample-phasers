8d2b47b7d0bb79f9e4b554a2788478b15344a524
Adam Williams <adam@thewilliams.ws>
2013-12-11 14:22:53 -0500

    Remove unused frameworks.
    This allowed me to avoid dealing with missing databases.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
 require File.expand_path('../../env', __FILE__)
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
